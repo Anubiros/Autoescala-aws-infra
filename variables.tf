9887e773db35be0c9d915187ff54d575f2881d5f
Humberto Machado <hmaxsuel25@gmail.com>
2021-03-07 13:43:32 -0300

    Atualização de Variaveis

--- variables.tf
+++ variables.tf
@@ -13,25 +13,30 @@
 
 
 #Arquivo de Variaveis
+variable "token" {
+  type        = string
+  description = "Informe o Linode API Access Token"
+}
+variable "instances_name" {
+  type        = list(string)
+  default     = ["server-1"]
+  description = "Nome da(s) instância(s)"
+}
 variable "region" {
-  description = "Sao Paulo"
-  default = "sa-east1"
+  type    = string
+  default = "us-southeast"
 }
-
-variable "key_path" {
-  description = "Public key path"
-  default = "~/.ssh/id_rsa.pub"
+variable "root_pass" {
+  type        = string
+  description = "Informe a senha do usuário root"
 }
-
-# Variavel ami antiga
-variable "ami" {
-  description = "AMI"
-  default = "ami-dff5d1b3" // Ubuntu 14.04
+variable "authorized_keys" {
+  type        = list(string)
+  default     = []
+  description = "Zero ou mais chaves RSA para autenticação SSH"
 }
-
-
-
-variable "instance_type" {
-  description = "EC2 instance type"
-  default = "t2.micro"
+variable "identification_tags" {
+  type        = list(string)
+  default     = []
+  description = "Zero ou mais tags utilizadas para identificação do elemento"
 }
